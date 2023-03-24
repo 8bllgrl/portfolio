83aad2e2eaff147bc52427839259faa0e005bdeb
Sage Belknap <sage.belknap@mykenzie.snhu.edu>
2023-03-24 17:46:13 -0600

    added more images

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -10,13 +10,13 @@ import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/l
 export class GalleryComponent {
 
 
+  // TODO: Better way of organizing this.
   imageUrls: string[] = [
     'https://i.imgur.com/Gzb9fYF.png',
     'https://i.imgur.com/57BvRnf.png',
     'https://i.imgur.com/3yIkgP2.png',
     'https://i.imgur.com/X68LxfY.png',
     'https://i.imgur.com/7x5JJxS.png',
-    'https://i.imgur.com/3Xi3d5k.png',
     'https://i.imgur.com/2lKAIFm.png',
     'https://i.imgur.com/8nYqppS.png',
     'https://i.imgur.com/WQtkMMC.png',
@@ -28,6 +28,40 @@ export class GalleryComponent {
     'https://picsum.photos/id/240/200/300',
     'https://picsum.photos/id/241/200/300',
     'https://picsum.photos/id/242/200/300',
+    'https://i.imgur.com/sWUT4lE.png',
+    'https://i.imgur.com/c6YWT9S.png',
+    'https://i.imgur.com/JawRP0L.png',
+    'https://i.imgur.com/nb2PWO0.png',
+    'https://i.imgur.com/IKtMHTW.png',
+    'https://i.imgur.com/2f76FAl.png',
+    'https://i.imgur.com/S07gZqt.png',
+    'https://i.imgur.com/Qgau0Nq.png',
+    'https://i.imgur.com/OR5wbtz.png',
+    'https://i.imgur.com/TgIyS66.png',
+    'https://i.imgur.com/yMvJFWQ.png',
+    'https://i.imgur.com/JNMmaAJ.png',
+    'https://i.imgur.com/Tq2BbNs.png',
+    'https://i.imgur.com/oyla7ux.gif',
+    'https://i.imgur.com/euxBH9r.png',
+    'https://i.imgur.com/iHEPr1G.png',
+    'https://i.imgur.com/oRKkygS.png',
+    'https://i.imgur.com/fZWslqw.png',
+    'https://i.imgur.com/genhkEd.png',
+    'https://i.imgur.com/RYMRCm4.png',
+    'https://i.imgur.com/HbNY00B.png',
+    'https://i.imgur.com/tQJ3tYY.png',
+    'https://i.imgur.com/H6bygle.png',
+    'https://i.imgur.com/i9KHF63.jpeg',
+    'https://i.imgur.com/QIooNv9.png',
+    'https://i.imgur.com/sFH7pCK.png',
+    'https://i.imgur.com/27FNGOR.png',
+    'https://i.imgur.com/KJ6H41v.png',
+    'https://i.imgur.com/Tx20AIT.png',
+    'https://i.imgur.com/uY0WFtq.png',
+    'https://i.imgur.com/cQqF9SQ.jpeg',
+    'https://i.imgur.com/2HfRsb4.jpeg',
+    'https://i.imgur.com/fRrAIPy.png',
+
     // Add more image URLs here...
   ];
 
@@ -38,28 +72,28 @@ export class GalleryComponent {
   ngOnInit(): void {
     const breakpoints = [Breakpoints.XSmall, Breakpoints.Small, Breakpoints.Medium, Breakpoints.Large, Breakpoints.XLarge];
     const images = Array.from(document.getElementsByClassName('gallery-image')) as HTMLElement[];
-    
+
     this.breakpointObserver.observe(breakpoints).subscribe(result => {
       this.columns = Object.keys(result.breakpoints).findIndex(breakpoint => result.breakpoints[breakpoint]) + 1;
-      
+
       const rowWidth = window.innerWidth - (this.columns * 10);
       let totalWidth = 0;
       let imagesInRow = 0;
-      
+
       images.forEach((image, i) => {
         image.style.width = 'auto';
         image.style.height = 'auto';
         totalWidth += image.offsetWidth;
-        
+
         if (totalWidth > rowWidth) {
-          (images[i].previousElementSibling as HTMLElement).style.marginRight = '0'; 
+          (images[i].previousElementSibling as HTMLElement).style.marginRight = '0';
           return;
         }
-        
+
         imagesInRow++;
         (images[i].previousElementSibling as HTMLElement).style.marginRight = '5px';
       });
-      
+
       if (imagesInRow > 0) {
         this.columns = imagesInRow;
       }
